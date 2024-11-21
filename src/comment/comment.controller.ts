dc2b5367b4a476d340e66778fba450fe0b4c9fce
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-11-20 23:54:29 -0300

    FEAT CRUD comentário

---
+++ src/comment/comment.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Post, ValidationPipe, Get, Param, ParseIntPipe, Delete, Patch } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+
+@Controller('comment')
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Post()
+  async create(@Body(ValidationPipe) commentData: CreateCommentDto){
+    return await this.commentService.create(commentData);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.commentService.findAll();
+  }
+
+  @Get(':id')
+  async findComment(@Param('id', ParseIntPipe) id: number){
+    return await this.commentService.findComment(id);
+    
+  }
+
+  @Delete(':id')
+  async deleteComment(@Param('id', ParseIntPipe) id: number){
+    return await this.commentService.deleteComment(id);
+
+  }
+
+  @Patch(':id')
+  async updateComment(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) data: UpdateCommentDto){
+    return await this.commentService.updateComment(id, data);
+    
+  }
+
+}
