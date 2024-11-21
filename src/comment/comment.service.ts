26eda744e095d128f1fcc98b9d6de8c122f9e88b
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-11-20 23:54:57 -0300

    FEAT CRUD comentário

---
+++ src/comment/comment.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+
+@Injectable()
+export class CommentService {
+    constructor(
+        private readonly prisma: PrismaService
+    ) {}
+
+    async create(data: CreateCommentDto){
+        const comment = this.prisma.comment.create({
+            data: {
+                ...data, 
+                isEdited: false, 
+            }
+        });
+        return comment;
+    }
+
+    async findAll(){
+        return await this.prisma.comment.findMany()
+    }
+
+    async findComment(id: number){
+        return await this.prisma.comment.findUnique({
+            where: {
+                id: id,
+            }
+        })
+    }
+
+    async deleteComment(id: number){
+        return await this.prisma.comment.delete({
+            where: {
+                id: id,
+            }
+        })
+    }
+
+    async updateComment(id: number, data: UpdateCommentDto){
+        return await this.prisma.comment.update({
+            where: {
+                id: id
+            },
+            data: {
+                ...data,
+                isEdited: true, 
+              },
+        })
+    }
+}
