001cc4c4401739707cf3b3a92f097c4615f1f5da
Henrique Sales <144968260+hsaless@users.noreply.github.com>
2024-11-20 23:53:11 -0300

    FEAT update comment DTO

--- src/comment/dto/update-comment.dto.ts
+++ src/comment/dto/update-comment.dto.ts
@@ -1,7 +1,9 @@
-import { IsBoolean, IsDate, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class UpdateCommentDto {
     @IsString()
+    @IsOptional()
+    @IsNotEmpty()
     text: string;  
   
    
