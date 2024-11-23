3443472e46650b49bd2a4f059fcc6d7ba4b99a9f
Marquito-2003 <marcoaureliofonseca2003@gmail.com>
2024-11-23 08:21:29 -0300

    feat: implementação CRUD de avaliação

---
+++ src/avaliacao/avaliacao.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAvaliacaoDto } from './dto/create-avaliacao.dto';
+import { UpdateAvaliacaoDto } from './dto/update-avaliacao.dto';
+
+
+@Injectable()
+export class AvaliacaoService  {
+    constructor(private readonly prisma : PrismaService){}
+        async create(data: CreateAvaliacaoDto){
+            const avaliacao = await this.prisma.avaliacao.create({
+                data:data,
+            });
+            return avaliacao;
+        }
+        async findAll(){
+            return await this.prisma.avaliacao.findMany()
+        }
+        async findAvaliacao(id: number){
+            return await this.prisma.avaliacao.findUnique({
+                where: {
+                    id: id,
+                }
+            })
+        }
+        async deleteAvaliacao(id:number){
+            return await this.prisma.avaliacao.delete({
+                where: {
+                    id: id,
+                }
+            })
+            
+        }
+        async updateAvaliacao(id:number, data: UpdateAvaliacaoDto){
+            return await this.prisma.avaliacao.update({
+                where: {
+                    id: id,
+                },
+                data: data,
+            })
+        }
+
+}
---
+++ src/avaliacao/dto/create-avaliacao.dto.ts
@@ -0,0 +1,27 @@
+import {
+    IsBoolean,
+    IsDate,
+    IsNumber,
+    IsOptional,
+    IsString,
+  } from 'class-validator';
+import { Transform } from 'class-transformer';
+export class CreateAvaliacaoDto{
+    @IsString()
+    text: string;
+
+    @IsNumber()
+    userId2: number;
+
+    @IsNumber()
+    nota: number;
+
+    @IsOptional()
+    @Transform(({ value }) => (value ? new Date(value) : undefined))
+    @IsDate()
+    date?: Date;
+
+    @IsOptional()
+    @IsBoolean()
+    isEdited: boolean;
+}
