ce36cb8e4708d341eb9967be9be93c529e183970
gabrielcaixeta01 <gabrielcaixeta@Laptop-de-Gabriel.local>
2024-12-10 20:04:12 -0300

    :bug: PORTA 4000 finalmente configurado e funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { CommentModule } from './comment/comment.module';
 import { AvaliacaoModule } from './avaliacao/avaliacao.module';
 import { ProfessorsModule } from './professors/professors.module';
 import { CoursesModule } from './courses/courses.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     PrismaModule,
     CommentModule,
